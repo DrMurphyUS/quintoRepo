c864d6610e3851064384a0dc8ecc920cd8058c2b
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-01 17:37:23 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Murphy Sequeiros",                         # Tu nombre
+    author_email="medicosequeiros@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/DrMurphyUS/quintoRepo",     # URL del proyecto
+)
